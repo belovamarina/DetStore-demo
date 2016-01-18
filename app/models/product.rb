6fbbbd1e5ff21083ba664bb2ca7ae080c949dd38
belovamarina <belova.ma.ev@gmail.com>
2016-01-18 21:06:51 +0300

    Add LineItem assiciations

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,25 @@ class Product < ActiveRecord::Base
   validates_attachment_size :picture, :less_than => 5.megabytes
   validates_attachment_content_type :picture, :content_type => ['image/jpeg', 'image/png']
   belongs_to :category
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
+
+
+  has_many :line_items
+  has_many :orders, through: :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+ # убеждаемся в отсутствии товарных позиций, ссылающихся на данный товар
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'line items exist')
+      return false
+    end
+  end
 end
