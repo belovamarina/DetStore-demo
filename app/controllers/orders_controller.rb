be481221b1116801113aa17f8ce83f9d4d21c6c0
belovamarina <belova.ma.ev@gmail.com>
2016-01-25 11:57:48 +0300

    Update form for Order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   include CurrentCart
-  before_action :set_cart, only: [:new, :create]
+  before_action :set_cart, only: [:new, :create, :update, :edit]
   before_action :set_order, only: [:show, :edit, :update, :destroy]
 
   # GET /orders
@@ -38,7 +38,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-        # OrderNotifier.received(@order).deliver
+        OrderNotifier.received(@order).deliver_now
         format.html { redirect_to store_url, notice: 'Thank you for your order.' }
         format.json { render :show, status: :created, location: @order }
       else
