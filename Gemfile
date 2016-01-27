3819a4173478981c4672bc6a928c9cb63907608c
belovamarina <belova.ma.ev@gmail.com>
2016-01-27 21:57:16 +0300

    Add and config Devise

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+gem 'devise'
+
 gem 'paperclip', '~> 4.3'
 gem 'bootstrap-sass', '~> 3.3.6'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :galleries
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
+
   resources :orders
   resources :line_items
   resources :carts
