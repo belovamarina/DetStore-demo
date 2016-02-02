c21efef0c2d0e89df032a5bc0f38c01eb41a1fd8
belovamarina <belova.ma.ev@gmail.com>
2016-02-02 14:23:18 +0300

    Update models (save picture in S3)

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,9 @@
 class Category < ActiveRecord::Base
   validates :title, presence: true
   
-  has_attached_file :picture, :styles => { small: "150x150>", medium: "300x300>", thumb: "100x100>" },
-                  :url  => "/assets/categories/:id/:style/:basename.:extension",
-                  :path => ":rails_root/public/assets/categories/:id/:style/:basename.:extension"
+  has_attached_file :picture, :styles => { small: "150x150>", medium: "300x300>", thumb: "100x100>" }
+                  # :url  => "/assets/categories/:id/:style/:basename.:extension",
+                  # :path => ":rails_root/public/assets/categories/:id/:style/:basename.:extension"
 
   #validates_attachment_presence :picture
   validates_attachment_size :picture, :less_than => 5.megabytes
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,9 +1,9 @@
 class Picture < ActiveRecord::Base
   belongs_to :gallery
 
-  has_attached_file :image, :styles => { small: "150x150>", medium: "300x300>", thumb: "100x100>" },
-                  :url  => "/assets/galleries/:id/:style/:basename.:extension",
-                  :path => ":rails_root/public/assets/galleries/:id/:style/:basename.:extension"
+  has_attached_file :image, :styles => { small: "150x150>", medium: "300x300>", thumb: "100x100>" }
+                  # :url  => "/assets/galleries/:id/:style/:basename.:extension",
+                  # :path => ":rails_root/public/assets/galleries/:id/:style/:basename.:extension"
 
   validates_attachment_size :image, :less_than => 5.megabytes
   validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png']
